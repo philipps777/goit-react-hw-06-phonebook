eb40ad546f145a523993de75715457758805064f
philips777 <mymailforall@ukr.net>
2023-10-04 00:14:57 +0300

    delite

--- src/components/ContactList/ContactItem.jsx
+++ src/components/ContactList/ContactItem.jsx
@@ -26,11 +26,3 @@ ContactItem.propTypes = {
   name: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired,
 };
-
-// import { Li, ButtonList, Number } from './ContactList.styled';
-// export const ContactItem = ({ contact, onDelete }) => (
-//   <Li>
-//     {contact.name}: <Number>{contact.number}</Number>
-//     <ButtonList onClick={() => onDelete(contact.id)}>Delete</ButtonList>
-//   </Li>
-// );

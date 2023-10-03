65ddb3f329c1ed581e3c0e015119916bb297c8fe
philips777 <mymailforall@ukr.net>
2023-10-04 00:28:21 +0300

    fix bugs

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -21,13 +21,4 @@ export const ContactList = () => {
     </List>
   );
 };
-// import { List } from './ContactList.styled';
-// import { ContactItem } from './ContactItem';
 
-// export const ContactList = ({ contacts, onDelete }) => (
-//   <List>
-//     {contacts.map(contact => (
-//       <ContactItem key={contact.id} contact={contact} onDelete={onDelete} />
-//     ))}
-//   </List>
-// );
